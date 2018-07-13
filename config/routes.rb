d6fdeeae0e86ea6a309236b8717cbc19d57eddda
kapil <kapilbhosale@hotmail.com>
2018-07-13 18:20:03 +0530

    menu changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :students do
     root to: 'home#index'
-    get :tests, to: 'home#tests'
+    get :tests, to: 'home#tests', as: :tests
     get :instructions, to: 'home#instructions', as: :exam_instructions
     get :confirmation, to: 'home#confirmation', as: :exam_confirmation
     get :subscription, to: 'home#subscription', as: :exam_subscription
