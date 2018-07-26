59c1c35f1659c2432228a5d935d1d39c63cb9b57
kapil <kapilbhosale@hotmail.com>
2018-07-26 10:35:48 +0530

    zip gem added to unzip the question paper html

--- Gemfile
+++ Gemfile
@@ -51,6 +51,9 @@ gem 'mini_magick'
 gem 'wicked_pdf'
 gem 'wkhtmltopdf-binary'
 
+gem 'rubyzip', '>= 1.0.0' # will load new rubyzip version
+gem 'zip-zip' # will load compatibility for old rubyzip API.
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/admin/exams_controller.rb
+++ app/controllers/admin/exams_controller.rb
@@ -5,12 +5,12 @@ class Admin::ExamsController < Admin::BaseController
   end
 
   def new
-
   end
 
   def create
     @exam = Exam.new(exam_params)
     if @exam.save
+      extract_zip(params[:questions_zip].tempfile) if params[:questions_zip].present?
       redirect_to admin_exams_path
     else
       render 'new'
@@ -18,7 +18,33 @@ class Admin::ExamsController < Admin::BaseController
   end
 
   private
+
   def exam_params
     params.permit(:name, :description, :no_of_questions, :time_in_minutes)
   end
+
+  def extract_zip(tmp_zip_file)
+    zip_name = "zip_#{Time.now.to_i}"
+    zip_file_path = "#{Rails.root}/tmp/#{zip_name}.zip"
+
+    # remove old data zip and csv files
+    # FileUtils.rm_rf(Dir.glob("#{Rails.root}/zip_data/*.csv"))
+    # FileUtils.rm_rf(Dir.glob("#{Rails.root}/zip_data/*.zip"))
+
+    FileUtils.mv tmp_zip_file, zip_file_path
+
+    Zip::ZipFile.open(zip_file_path) { |zip_file|
+      zip_file.each { |f|
+        f_path=File.join("#{Rails.root}/tmp/zip_data/", f.name)
+        FileUtils.mkdir_p(File.dirname(f_path))
+        zip_file.extract(f, f_path) {true}
+      }
+    }
+    binding.pry
+  end
+
 end
+
+# Imgur
+# client_id : 5b8da04bb57e141
+# secret : ad9a52bcab65dbd79b68c2b98b53eec9c1fbecb6
