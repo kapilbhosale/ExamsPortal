2dc53f2c979c08ff681d48530b08055d48d00212
kapil <kapilbhosale@hotmail.com>
2018-07-28 00:26:10 +0530

    removed static style

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2018_07_27_153956) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "hstore"
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
