41a2cbd32fd5b06a4218dae8f02d04dbc3f21d69
Ranjeet Jagtap <ranjeet.jagtap43@gmail.com>
2018-07-27 19:55:10 +0530

    sign out issue solved for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(student)
       if current_admin
-        '/admin'
+        '/admin/reports'
       else
         '/students'
       end
