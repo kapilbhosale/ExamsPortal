5e58b4f225712377a326a4133c0b9c91f29a1211
kapil <kapilbhosale@hotmail.com>
2018-07-28 16:45:20 +0530

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_07_28_082413) do
-
+ActiveRecord::Schema.define(version: 2018_07_28_081314) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "hstore"
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
