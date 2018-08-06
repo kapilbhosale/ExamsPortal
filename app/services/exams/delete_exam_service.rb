f2cb1900c06f8029dea046c2a89d18c65942ac27
Ranjeet Jagtap <ranjeet.jagtap43@gmail.com>
2018-08-06 17:15:38 +0530

    DeleteExamService added

---
+++ app/services/exams/delete_exam_service.rb
@@ -0,0 +1,29 @@
+class DeleteExamError < StandardError; end
+
+module Exams
+  class DeleteExamService
+    attr_reader :exam
+    def initialize(exam_id)
+      @exam = Exam.find_by(id: exam_id)
+    end
+
+    def delete
+      validate_request
+      exam.destroy!
+      return {status: true, message: 'Exam deleted successfully'}
+    rescue DeleteExamError, ActiveRecord::RecordInvalid => ex
+      return {status: false, message: ex.message}
+    end
+
+    private
+
+    def validate_request
+      raise DeleteExamError, 'Exam does not exists!' unless exam.present?
+      raise DeleteExamError, 'Exam can\'t be deleted because some students have already appeared for it!' if students_appeared?
+    end
+
+    def students_appeared?
+      StudentExam.where(exam_id: exam.id).present?
+    end
+  end
+end
