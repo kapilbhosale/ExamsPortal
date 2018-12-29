1ed68cce1bc5c3c20d8b5fc281dcdc66b2240a31
kapil <kapilbhosale@hotmail.com>
2018-12-29 22:23:00 +0530

    rollbar removed

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,6 +1,6 @@
-Rollbar.configure do |config|
-  config.access_token = ENV["ROLLBAR_ACCESS_TOKEN"]
-  unless Rails.env.production?
-    config.enabled = false
-  end
-end
+# Rollbar.configure do |config|
+#   config.access_token = ENV["ROLLBAR_ACCESS_TOKEN"]
+#   unless Rails.env.production?
+#     config.enabled = false
+#   end
+# end
