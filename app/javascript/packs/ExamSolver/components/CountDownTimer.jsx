dfd0ecace2a4193bcfca60b4d6f63250f8b0de3e
kapil <kapilbhosale@hotmail.com>
2020-03-06 11:15:52 +0530

    changes in time to sync exam

--- app/javascript/packs/ExamSolver/components/CountDownTimer.jsx
+++ app/javascript/packs/ExamSolver/components/CountDownTimer.jsx
@@ -8,7 +8,7 @@ class CountDownTimer extends React.Component {
         // Syncing answers with backend every 20 Seconds
         this.syncInterval = setInterval(() => {
             this.props.syncWithBackend();
-        }, 40000);
+        }, (2*60*1000));
     }
 
     componentWillUnmount() {
