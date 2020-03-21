ca06cd3871624523c273550223641b779edb1b13
kapil <kapilbhosale@hotmail.com>
2020-03-21 15:17:03 +0530

    error fixed

--- db/seeds/N-B3.rb
+++ db/seeds/N-B3.rb
@@ -1,15 +1,15 @@
 batch = Batch.find_or_create_by(name: 'JEE')
 students = [
-  "101,Demo user,1234567890",
-  "102,Demo user,1234567890",
-  "103,Demo user,1234567890",
-  "104,Demo user,1234567890",
-  "105,Demo user,1234567890",
-  "106,Demo user,1234567890",
-  "107,Demo user,1234567890",
-  "108,Demo user,1234567890",
-  "109,Demo user,1234567890",
-  "110,Demo user,1234567890",
+  "111,Demo user,1234567890",
+  "112,Demo user,1234567890",
+  "113,Demo user,1234567890",
+  "114,Demo user,1234567890",
+  "115,Demo user,1234567890",
+  "116,Demo user,1234567890",
+  "117,Demo user,1234567890",
+  "118,Demo user,1234567890",
+  "119,Demo user,1234567890",
+  "120,Demo user,1234567890",
   ]
 
 students.each do |row|
