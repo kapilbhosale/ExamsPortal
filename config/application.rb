d2be94ac16661d42d7ed8f59d372b2b24068d383
kapil <kapilbhosale@hotmail.com>
2020-04-16 14:21:28 +0530

    favicon added

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module SmartExamsRails
     config.time_zone = 'Chennai'
     config.active_record.default_timezone = :local
 
+    config.middleware.insert_after ActionDispatch::Static, Rack::Deflater
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
