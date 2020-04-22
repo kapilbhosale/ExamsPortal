494517d6c5425473caa4435c471d52086288a3e5
kapil <kapilbhosale@hotmail.com>
2020-04-22 12:19:46 +0530

    banner and logo changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,15 @@ module ApplicationHelper
     content_for(:header) { text.to_s }
   end
 
+  def get_logo(subdomain)
+    if subdomain == 'exams' || subdomain == 'videos'
+      return { logo: 'rcc-logo', width: 50 }
+    elsif subdomain == 'konale-exams' || subdomain == 'konale-videos'
+      return { logo: 'konale-logo', width: 120 }
+    end
+    { logo: 'rcc-logo', width: 50 }
+  end
+
 	def distance_of_time_in_hours_and_minutes(from_time, to_time)
 	  from_time = from_time.to_time if from_time.respond_to?(:to_time)
 	  to_time = to_time.to_time if to_time.respond_to?(:to_time)
