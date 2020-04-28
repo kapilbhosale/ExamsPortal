688c0180e8d5561570d10a643594c7b168dacd89
kapil <kapilbhosale@hotmail.com>
2020-04-29 00:38:02 +0530

    saraswati mobile app changes

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::HomeController < Api::V1::ApiController
     json_data = {
       'top_banners_data' => top_banners_data,
       'bottom_list_data' => [],
-      'about_us_link' => 'https://rccpattern.com/',
+      'about_us_link' => 'https://saraswaticoachingclasses.in/',
       'tests_appeared_data' => {
         appeared: 12,
         total: 25,
@@ -23,27 +23,29 @@ class Api::V1::HomeController < Api::V1::ApiController
   def top_banners_data
     [
       {
-        img_url: 'https://rccpattern.com/assests/images/home-slider/RCC-Pattern_Motegaonkar_Sir_3.jpg',
-        on_click: 'https://rccpattern.com' },
+        img_url: 'https://smart-exams-production.s3.ap-south-1.amazonaws.com/apks/saraswati-banner-2.jpg',
+        on_click: 'https://saraswaticoachingclasses.in/'
+      },
       {
-        img_url: 'https://rccpattern.com/assests/images/home-slider/RCC-Pattern_Motegaonkar_Sir_6.jpg',
-        on_click: nil },
-      {
-        img_url: 'https://static01.nyt.com/images/2020/04/12/magazine/12Ethicist/12Ethicist-superJumbo.jpg',
-        on_click: 'https://www.youtube.com/watch?v=Hp0yIgdRf4Q' }
+        img_url: 'https://smart-exams-production.s3.ap-south-1.amazonaws.com/apks/sarawati-banner.jpg',
+        on_click: nil
+      },
     ]
   end
 
   def gallery
     json_data = [
       {
-        img_url: 'https://rccpattern.com/assests/images/home-slider/RCC-Pattern_Motegaonkar_Sir_3.jpg',
+        img_url: 'https://smart-exams-production.s3.ap-south-1.amazonaws.com/apks/saraswati-banner-2.jpg',
       },
       {
-        img_url: 'https://rccpattern.com/assests/images/home-slider/RCC-Pattern_Motegaonkar_Sir_6.jpg',
+        img_url: 'http://saraswaticoachingclasses.in/wp-content/uploads/2019/06/Screenshot-2019-06-10-at-8.55.43-AM.png',
       },
       {
-        img_url: 'https://static01.nyt.com/images/2020/04/12/magazine/12Ethicist/12Ethicist-superJumbo.jpg',
+        img_url: 'http://saraswaticoachingclasses.in/wp-content/uploads/2019/06/Screenshot-2019-06-09-at-9.06.51-AM.png',
+      },
+      {
+        img_url: 'http://saraswaticoachingclasses.in/wp-content/uploads/2018/06/gauri-thakre-akola.png'
       }
     ]
     render json: json_data, status: :ok
--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -5,18 +5,8 @@ class Api::V1::NotificationsController < Api::V1::ApiController
   def index
     json_data = [
       {
-        title: 'Title of Notification one',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        added_on: '27 April 2020'
-      },
-      {
-        title: 'Title of Notification two',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        added_on: '27 April 2020'
-      },
-      {
-        title: 'Title of Notification 3',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        title: 'Welcome to Online Exams and Video portals',
+        description: 'We are pleased to announce that, today we welcome all students to this online portal. We welcome your suggestions to improve your experience',
         added_on: '27 April 2020'
       }
     ]
