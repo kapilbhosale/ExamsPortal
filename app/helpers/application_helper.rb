753623de8e39e5169406609c5e5ed7b2e2813ee3
kapil <kapilbhosale@hotmail.com>
2020-05-02 15:13:22 +0530

    Pis and rathi changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,19 +8,21 @@ module ApplicationHelper
   end
 
   def get_logo(subdomain)
-    if subdomain == 'exams' || subdomain == 'videos'
+    if %w[exams videos].include? subdomain
       return { logo: 'rcc-logo', width: 50 }
-    elsif subdomain == 'konale-exams' || subdomain == 'konale-videos'
+    elsif %w[konale-exams konale-videos].include? subdomain
       return { logo: 'konale-logo', width: 120 }
-    elsif subdomain == 'dhote-exams' || subdomain == 'dhote-videos'
+    elsif %w[dhote-exams dhote-videos].include? subdomain
       return { logo: 'dhote-logo', width: 120 }
-    elsif subdomain == 'saraswati-exams' || subdomain == 'saraswati-videos'
+    elsif %w[saraswati-exams saraswati-videos].include? subdomain
       return { logo: 'saraswati-logo', width: 120 }
-    elsif subdomain == 'dayanand'
+    elsif %w[dayanand].include? subdomain
       return { logo: 'dayanand-logo', width: 45, height: 40 }
+    elsif %w[pis].include? subdomain
+      return { logo: 'ankush-logo', width: 45, height: 40 }
     end
 
-    { logo: 'rcc-logo', width: 50 }
+    { logo: 'exams-logo', width: 30 }
   end
 
   def get_header_name(subdomain)
@@ -29,6 +31,8 @@ module ApplicationHelper
     if subdomain == 'dhote-exams' || subdomain == 'dhote-videos'
       return request.subdomain.split('-').last.humanize
     end
+
+    return 'PIS, Jalna' if ['pis'].include? subdomain
 
     request.subdomain.humanize || "Exams"
   end
