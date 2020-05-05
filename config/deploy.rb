2f1f2b3d0f9429ed6e1dc929b170489a9776a13a
kapil <kapilbhosale@hotmail.com>
2020-05-06 00:56:43 +0530

    develop script changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,8 +44,8 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/clinet-independent-branch`
-        puts "WARNING: HEAD is not the same as origin/clinet-independent-branch"
+      unless `git rev-parse HEAD` == `git rev-parse origin/develop`
+        puts "WARNING: HEAD is not the same as origin/develop"
         puts "Run `git push` to sync changes."
         exit
       end
