6e019cc2e2b1bbcbf7577d1e67e7197578446949
kapil <kapilbhosale@hotmail.com>
2020-05-05 15:08:51 +0530

    video thumbnail is fetched from vimeo

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -11,13 +11,7 @@ class Api::V1::VideosController < Api::V1::ApiController
     lectures_data = {}
     lectures.each do |lect|
       lect_data = lect.attributes.slice("id" ,"title", "url", "video_id", "description", "by", "tag", "subject", "video_type")
-      banner_url = lect.thumbnail.banner.url
-      if banner_url.include?('amazonaws.com')
-        video_name = banner_url.split('/').last
-        lect_data['thumbnail_url'] = "https://smart-exams-production.s3.ap-south-1.amazonaws.com/video-thumbnails/#{video_name}"
-      else
-        lect_data['thumbnail_url'] = "#{helpers.full_domain_path}#{banner_url}"
-      end
+      lect_data['thumbnail_url'] = lect.thumbnail
       lect_data['added_ago'] = helpers.time_ago_in_words(lect.created_at)
       if lect.vimeo?
         lect_data['play_url'] = "#{helpers.full_domain_path}/students/lectures/#{lect.video_id}"
