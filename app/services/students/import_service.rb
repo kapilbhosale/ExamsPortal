e813ba0eac1a068d122abaf3ab106537a5433c3b
kapil <kapilbhosale@hotmail.com>
2020-07-01 15:59:26 +0530

    xls import student mobile added

--- app/services/students/import_service.rb
+++ app/services/students/import_service.rb
@@ -15,20 +15,24 @@ module Students
       return { error: "Batch not found" } if batch.blank?
 
       CSV.open(@csv_file_path, :row_sep => :auto, :encoding => 'ISO-8859-1', :col_sep => ",") do |csv|
-        return { error: "must have only 3 columns" } if csv.first.length != 3
+        return { error: "must have only 4 columns" } if csv.first.length != 4
+
         csv.each do |row|
           roll_number = row[0]&.strip
           name = row[1]&.strip
           rand_password = row[2]&.strip || '1'
-          email = "#{roll_number}-client-#{batch.name.parameterize}@se.com"
+          rand_password = '111111' if rand_password.length < 6
+          student_mobile = row[3]&.strip || '1234567890'
+
+          email = "#{roll_number}-#{rand_password}@se.com"
           student = Student.find_or_initialize_by(email: email)
           student.roll_number = roll_number
           student.name = name
 
-          rand_password = '111111' if rand_password.length < 6
           student.password = rand_password
           student.raw_password = rand_password
           student.parent_mobile = rand_password
+          student.student_mobile = student_mobile
 
           student.org = org
           student.save
