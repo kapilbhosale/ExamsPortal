54b1fce2d7af5f2c9ab83b2ced0b062a3b57c774
kapil <kapilbhosale@hotmail.com>
2020-07-31 16:51:30 +0530

    Zoom issue fixed

--- app/controllers/api/v1/meetings_controller.rb
+++ app/controllers/api/v1/meetings_controller.rb
@@ -3,11 +3,15 @@
 class Api::V1::MeetingsController < Api::V1::ApiController
 
   def index
-    all_meetings = BatchZoomMeeting.includes(:zoom_meeting).where(batch: current_student.batches).map(&:zoom_meeting)
+    all_meetings = ZoomMeeting
+      .includes(:batch_zoom_meetings)
+      .where(batch_zoom_meetings: {batch_id: current_student.batches.ids})
+      .where("datetime_of_meeting > '#{Time.now.beginning_of_day}' and datetime_of_meeting < '#{Time.now.beginning_of_day + 2.days}'")
+
     upcoming_meeting = ZoomMeeting
       .includes(:batch_zoom_meetings)
       .where(batch_zoom_meetings: {batch_id: current_student.batches.ids})
-      .where("zoom_meetings.datetime_of_meeting > '#{Time.current}' and zoom_meetings.datetime_of_meeting < '#{Time.current + 1.hour}'")
+      .where("datetime_of_meeting > '#{Time.current - 30.minutes}' and datetime_of_meeting < '#{Time.current + 1.hour}'").first
 
     meetings_data = {
       zoom_app_key: '5oNVTIJJEMjDO4HiQuk0Ao476WcnHwTD43wB',
