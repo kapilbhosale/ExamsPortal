93eeea708d5087a3c25dd5c533aa7181e563f576
kapil <kapilbhosale@hotmail.com>
2020-08-10 22:40:38 +0530

    meeting change

--- app/controllers/api/v1/meetings_controller.rb
+++ app/controllers/api/v1/meetings_controller.rb
@@ -6,12 +6,16 @@ class Api::V1::MeetingsController < Api::V1::ApiController
     all_meetings = ZoomMeeting
       .includes(:batch_zoom_meetings)
       .where(batch_zoom_meetings: {batch_id: current_student.batches.ids})
-      .where("datetime_of_meeting > '#{Time.now.beginning_of_day}' and datetime_of_meeting < '#{Time.now.beginning_of_day + 2.days}'")
+      .where("datetime_of_meeting > ?", Time.now.beginning_of_day)
+      .where("datetime_of_meeting < ?", Time.now.beginning_of_day + 2.days)
+      .order(id: :desc)
 
     upcoming_meeting = ZoomMeeting
       .includes(:batch_zoom_meetings)
       .where(batch_zoom_meetings: {batch_id: current_student.batches.ids})
-      .where("datetime_of_meeting > '#{Time.current - 30.minutes}' and datetime_of_meeting < '#{Time.current + 1.hour}'").first
+      .where("datetime_of_meeting > ?", Time.current - 30.minutes)
+      .where("datetime_of_meeting <= ?", Time.current + 1.hour)
+      .first
 
     meetings_data = {
       zoom_app_key: '5oNVTIJJEMjDO4HiQuk0Ao476WcnHwTD43wB',
