6d97e3e34a478542e141efb40bdc79d5e34e9711
kapilbhosale <kapilbhosale@hotmail.com>
2020-08-12 13:39:03 +0530

    PDF fix

--- app/controllers/api/v1/study_pdfs_controller.rb
+++ app/controllers/api/v1/study_pdfs_controller.rb
@@ -7,7 +7,8 @@ class Api::V1::StudyPdfsController < Api::V1::ApiController
     study_pdfs = StudyPdf.includes(:study_pdf_type).where(id: study_pdf_ids).where(org: current_org).order(id: :desc)
     json_data = {}
     study_pdfs.each do |study_pdf|
-      json_data[study_pdf.study_pdf_type.name] = {
+      json_data[study_pdf.study_pdf_type.name] ||= []
+      json_data[study_pdf.study_pdf_type.name] << {
         name: study_pdf.name,
         description: study_pdf.description,
         added_on: study_pdf.created_at.strftime("%d-%B-%Y %I:%M%p"),
