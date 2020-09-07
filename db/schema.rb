afb3c4bb92e752fcc42fa22dabac8eb0c8e6a9cd
kapilbhosale <kapilbhosale@hotmail.com>
2020-09-07 12:49:36 +0530

    annotation changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_09_05_051319) do
+<<<<<<< HEAD
+=======
+
+>>>>>>> annotation changes
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
