ca995ecece1b167bf1ba5ddd57f8c72c35366997
kapilbhosale <kapilbhosale@hotmail.com>
2020-09-16 17:32:55 +0530

    Repeater admission changes

--- app/controllers/students/admissions_controller.rb
+++ app/controllers/students/admissions_controller.rb
@@ -126,6 +126,16 @@ class Students::AdmissionsController < ApplicationController
       return 21_000 if course.name == "cb"
       return 33_000 if course.name == "pcb"
     end
+    if batch == 'repeater'
+      return 12_000 if course.name == "phy"
+      return 12_000 if course.name == "chem"
+      return 12_000 if course.name == "bio"
+
+      return 24_000 if course.name == "pc"
+      return 24_000 if course.name == "pb"
+      return 24_000 if course.name == "cb"
+      return 30_000 if course.name == "pcb"
+    end
     course.fees
   end
 
@@ -160,8 +170,13 @@ class Students::AdmissionsController < ApplicationController
               student.save
               student.batches << batches
               # student.roll_number = suggest_online_roll_number(Org.first, batches, true)
-              student.roll_number = suggest_tw_online_roll_number
-              student.suggested_roll_number = suggest_tw_online_roll_number
+              if @new_admission.batch == 'repeater'
+                student.roll_number = suggest_rep_online_roll_number
+                student.suggested_roll_number = suggest_rep_online_roll_number
+              else
+                student.roll_number = suggest_tw_online_roll_number
+                student.suggested_roll_number = suggest_tw_online_roll_number
+              end
               student.api_key = student.api_key + '+1'
               student.app_login = false
               student.save
@@ -194,13 +209,22 @@ class Students::AdmissionsController < ApplicationController
 
   private
 
-  INITIAL_TW_ROLL_NUMBER = 51200
+    INITIAL_TW_ROLL_NUMBER = 51200
     def suggest_tw_online_roll_number
       online_s_ids = NewAdmission.where(error_code: ['E000', 'E006']).where(batch: NewAdmission.batches['12th']).where.not(student_id: nil).pluck(:student_id)
       rn = Student.where(id: online_s_ids).where.not(new_admission_id: nil).pluck(:suggested_roll_number).reject(&:blank?).max rescue nil
       return rn + 1 if rn
 
       INITIAL_TW_ROLL_NUMBER
+    end
+
+    INITIAL_RP_ROLL_NUMBER = 6001
+    def suggest_rep_online_roll_number
+      online_s_ids = NewAdmission.where(error_code: ['E000', 'E006']).where(batch: NewAdmission.batches['repeater']).where.not(student_id: nil).pluck(:student_id)
+      rn = Student.where(id: online_s_ids).where.not(new_admission_id: nil).pluck(:suggested_roll_number).reject(&:blank?).max rescue nil
+      return rn + 1 if rn
+
+      INITIAL_RP_ROLL_NUMBER
     end
 
     INITIAL_ONLINE_ROLL_NUMBER = 1100
@@ -317,6 +341,13 @@ class Students::AdmissionsController < ApplicationController
           return Batch.where(name: 'B-2_Nanded_11th_PB_2020') if course.name == 'pb'
           return Batch.where(name: 'B-2_Nanded_11th_CB_2020') if course.name == 'cb'
         end
+      elsif batch == 'repeater'
+        org = Org.first
+        batch_name = rcc_branch == "latur" ?
+          "Latur-REP-#{course.name.upcase}-2020" :
+          "Nanded-REP-#{course.name.upcase}-2020"
+
+        return Batch.find_or_create_by(org_id: org.id, name: batch_name)
       else
         if rcc_branch == "latur"
           return Batch.where(name: 'Ltr_12th-PCB_2020-21') if course.name == 'pcb'
