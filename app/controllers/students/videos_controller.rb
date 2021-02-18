915fcd64606a317209c4572e75d5db5772419e70
kapilbhosale <kapilbhosale@hotmail.com>
2021-02-18 22:03:25 +0530

    video lectures for laptop change

--- app/controllers/students/videos_controller.rb
+++ app/controllers/students/videos_controller.rb
@@ -7,13 +7,12 @@ class Students::VideosController < Students::BaseController
   end
 
   def category_videos
-    lectures = VideoLecture
-      .where(org: current_org)
-      .includes(:batches)
-      .where(batches: {id: current_student.batches.ids}, genre_id: params[:id].to_i)
-      .where(enabled: true)
-      .where.not(laptop_vimeo_id: nil)
-      .order(id: :desc)
+    all_vls = VideoLecture.includes(:batches)
+    .where(org_id: current_org.id)
+    .where(batches: {id: current_student.batches.ids})
+    .where.not(laptop_vimeo_id: nil)
+
+    lectures = all_vls.where(enabled: true).order(id: :desc)
 
     @lectures_data = {}
     lectures.each do |lect|
@@ -22,18 +21,81 @@ class Students::VideosController < Students::BaseController
       @lectures_data[lect.subject&.name] ||= []
       @lectures_data[lect.subject&.name] << lect
     end
+
+    disabled_lectures = all_vls.where(enabled: false).order(id: :desc)
+
+    disabled_lectures.each do |vl|
+      next if vl.publish_at.present? && vl.publish_at > Time.current
+
+      svfs = StudentVideoFolder.where(student_id: current_student.id).index_by(&:genre_id)
+      svf = svfs[vl&.genre&.id]
+
+      include_flag = false
+      if svf.present? && svf.show_till_date.to_date >= Date.today
+        include_flag = true
+      else
+        genre_date = vl&.publish_at || vl&.created_at.to_date
+        student_active_date = current_student.created_at
+        include_flag = true if student_active_date > genre_date && student_active_date.to_date + 30.days >= Date.today
+      end
+
+      next unless include_flag
+
+      @lectures_data[lect.subject&.name] ||= []
+      @lectures_data[lect.subject&.name] << lect
+    end
   end
 
   def lectures
-    video_lectures = VideoLecture
+    all_vls = VideoLecture
       .includes(:genre, :subject, :batches)
       .where(org_id: current_org.id)
       .where(batches: { id: current_student.batches.ids })
-      .where(enabled: true)
       .where.not(laptop_vimeo_id: nil)
+
+    video_lectures = all_vls.where(enabled: true)
     @categories_data = {}
-    video_lectures.all.each do |vl|
+
+    if current_org.subdomain == 'exams' && current_student.pending_amount.present? && current_student.block_videos?
+      render json: {}, status: :ok and return
+    end
+
+    video_lectures.each do |vl|
       next if vl.publish_at.present? && vl.publish_at > Time.current
+
+      subject_name = vl.subject&.name
+      @categories_data[subject_name] ||= {}
+      @categories_data[subject_name][vl.genre_id] ||= {
+        id: vl.genre_id || 0,
+        name: vl&.genre&.name || 'Default',
+        count: 0,
+        new: 0
+      }
+      @categories_data[subject_name][vl.genre_id][:count] += 1
+      if vl.created_at >= Time.zone.now.beginning_of_day
+        @categories_data[subject_name][vl.genre_id][:new] += 1
+      end
+    end
+
+    # considering enalbed false videos for students admitted late.
+    # starts here
+    video_lectures = all_vls.where(enabled: false)
+    video_lectures.each do |vl|
+      next if vl.publish_at.present? && vl.publish_at > Time.current
+
+      svfs = StudentVideoFolder.where(student_id: current_student.id).index_by(&:genre_id)
+      svf = svfs[vl&.genre&.id]
+
+      include_flag = false
+      if svf.present? && svf.show_till_date.to_date >= Date.today
+        include_flag = true
+      else
+        genre_date = vl&.publish_at || vl&.created_at.to_date
+        student_active_date = current_student.created_at
+        include_flag = true if student_active_date > genre_date && student_active_date.to_date + 30.days >= Date.today
+      end
+
+      next unless include_flag
 
       subject_name = vl.subject&.name
       @categories_data[subject_name] ||= {}
