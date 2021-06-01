030bb426b639483d11674748b0598a58f531d1e8
kapilbhosale <kapilbhosale@hotmail.com>
2021-06-02 01:21:27 +0530

    demo account added for vaa

--- app/controllers/api/v1/students_controller.rb
+++ app/controllers/api/v1/students_controller.rb
@@ -95,7 +95,7 @@ class Api::V1::StudentsController < Api::V1::ApiController
 
   def demo_account?(student)
     return true if student.roll_number == 101 && student.parent_mobile == '999999'
-
+    return true if student.roll_number == 100 && student.parent_mobile == '1234567890'
     return true if student.roll_number == 1001 && student.parent_mobile == '999999'
 
     false
