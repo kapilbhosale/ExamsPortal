27b72e375c203e070ded73c12f29142b9d6643d7
kapilbhosale <kapilbhosale@hotmail.com>
2022-08-12 11:19:38 +0530

    rcc deploy script changes

--- config/deploy/rcc_web.rb
+++ config/deploy/rcc_web.rb
@@ -4,4 +4,5 @@ set :sidekiq_env, :production
 set :stage, :production
 
 # server '13.126.111.72', user: 'ubuntu', roles: [:web, :app, :db], primary: true
-role [:web, :app, :db], %w{13.233.91.212 65.1.167.12}, user: 'ubuntu', primary: true
+# role [:web, :app, :db], %w{13.233.91.212 3.111.45.24 65.0.69.34 3.110.110.135 52.66.220.42 15.206.208.54 13.235.53.158}, user: 'ubuntu', primary: true
+role [:web, :app, :db], %w{3.111.45.24 65.0.81.61 43.205.99.45}, user: 'ubuntu', primary: true
