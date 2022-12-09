9b97233e60a3697148f96f0880de6f515a378c89
kapilbhosale <kapilbhosale@hotmail.com>
2022-12-09 12:04:15 +0530

    Attendance machines api changes

--- app/controllers/admin/api/attendance_controller.rb
+++ app/controllers/admin/api/attendance_controller.rb
@@ -10,8 +10,10 @@ class Admin::Api::AttendanceController < Admin::Api::ApiController
 
   def machines
     if params[:username] == "RCC@attendance" && params[:password] == "RandomPassword@758463"
-      att_machines = AttMachine.where(org_id: current_org.id, disabled: false)
-      render json: { machines: att_machines }, status: :ok
+      att_machines = AttMachine.where(org_id: current_org.id, disabled: false).map do |mac|
+        { ip: mac.ip_address, name: mac.name }
+      end
+      render json: { machines: att_machines }, status: :ok and return
     end
     render json: { machines: [] }, status: :ok
   end
