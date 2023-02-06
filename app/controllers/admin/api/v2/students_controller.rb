b020bae86070b7d8402984e082a4b81af59ab7f0
kapilbhosale <kapilbhosale@hotmail.com>
2023-02-07 01:41:14 +0530

    add new student changes

--- app/controllers/admin/api/v2/students_controller.rb
+++ app/controllers/admin/api/v2/students_controller.rb
@@ -7,11 +7,16 @@ class Admin::Api::V2::StudentsController < Admin::Api::V2::ApiController
     system_roll_number = Student.random_roll_number
     errors = []
 
-    if params[:parent_mobile].length != 10 || params[:student_mobile].length != 10
+    
+    errors << "In-valid batch" if params[:batch_id].blank?
+
+    if params[:parentMobile].to_s.length != 10 || params[:studentMobile].to_s.length != 10
       errors << "In-valid student or parent mobile"
     end
 
     errors << "In-valid batch" if params[:batch_id].blank?
+    batch = Batch.find(params[:batch_id])
+    errors << "In-valid batch" if batch.blank?
 
     render json: errors, status: :unprocessable_entity and return if errors.present?
 
@@ -20,8 +25,8 @@ class Admin::Api::V2::StudentsController < Admin::Api::V2::ApiController
       gender: params[:gender] == 'male' ? 0 : 1,
       email: "#{system_roll_number}@#{current_org.subdomain}.eduaakar.com",
       roll_number: system_roll_number,
-      password: params[:parent_mobile],
-      raw_password: params[:parent_mobile]
+      password: student_params[:parent_mobile],
+      raw_password: student_params[:parent_mobile]
     })
 
     student = Student.create(create_student_params)
@@ -30,21 +35,22 @@ class Admin::Api::V2::StudentsController < Admin::Api::V2::ApiController
       render json: student.errors.full_messages, status: :unprocessable_entity and return
     end
 
+    student.batches << batch
     render json: student
   end
 
   private
 
   def student_params
-    params.permit(
-      :name,
-      :mother_name,
-      :date_of_birth,
-      :ssc_marks,
-      :address,
-      :student_mobile,
-      :parent_mobile,
-      :category
-    )
+    {
+      name: params[:name],
+      mother_name: params[:mother_name],
+      date_of_birth: params[:date_of_birth],
+      ssc_marks: params[:ssc_marks],
+      address: params[:address],
+      student_mobile: params[:studentMobile],
+      parent_mobile: params[:parentMobile],
+      category: params[:category]
+    }
   end
 end
