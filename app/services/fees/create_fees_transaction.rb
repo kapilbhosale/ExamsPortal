5e6c26593eda025f456969d02a8b231d0e9994af
kapilbhosale <kapilbhosale@hotmail.com>
2023-02-06 20:12:06 +0530

    fees GST wrong calculations fix

--- app/services/fees/create_fees_transaction.rb
+++ app/services/fees/create_fees_transaction.rb
@@ -135,10 +135,10 @@ module Fees
       @sanitized_input_payments.each do |key, val|
         @fees_tempalte_data[key]
         total_gst = @fees_tempalte_data[key]['cgst'].to_f + @fees_tempalte_data[key]['sgst'].to_f
-        fees = val[:pay].to_f / ((100 + total_gst) / 100.0)
+        fees = val[:pay].to_f / ((100 + total_gst) / 100.0).round(2)
 
-        cgst = fees * (@fees_tempalte_data[key]['cgst'].to_f / 100.0)
-        sgst = fees * (@fees_tempalte_data[key]['sgst'].to_f / 100.0)
+        cgst = fees * (@fees_tempalte_data[key]['cgst'].to_f / 100.0).round(2)
+        sgst = fees * (@fees_tempalte_data[key]['sgst'].to_f / 100.0).round(2)
 
         total_paid += val[:pay].to_f
         total_cgst += cgst
