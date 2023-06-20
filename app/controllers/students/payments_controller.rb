4640b2e3671a8bd56425734d1dccbca80ffb3f07
kapilbhosale <kapilbhosale@hotmail.com>
2023-06-20 12:34:07 +0530

    admission error fixec

--- app/controllers/students/payments_controller.rb
+++ app/controllers/students/payments_controller.rb
@@ -58,7 +58,7 @@ class Students::PaymentsController < ApplicationController
     order_id = params.dig(:payload, :payment, :entity, :order_id) || params.dig('payload', 'payment', 'entity', 'order_id')
     new_admission = NewAdmission.find_by(rz_order_id: order_id)
     if new_admission && new_admission.success?
-      new_admission.payment_callback_data = params[:payment] || params[:payload][:payment]
+      new_admission.payment_callback_data = params[:payment] || params["payment"] || params
       new_admission.save
     end
   end
