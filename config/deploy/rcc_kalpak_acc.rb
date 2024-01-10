6123f8db2c6b0acec63c0b22bd6c097423454189
kapilbhosale <kapilbhosale@hotmail.com>
2024-01-11 03:05:40 +0530

    new kalpak server deployment changes

--- config/deploy/rcc_kalpak_acc.rb
+++ config/deploy/rcc_kalpak_acc.rb
@@ -3,5 +3,8 @@ set :branch, 'master'
 set :sidekiq_env, :production
 set :stage, :production
 
-# role [:web, :app, :db], %w{3.111.45.24 15.207.236.182 65.1.121.28 65.1.129.73 3.7.74.115 }, user: 'ubuntu', primary: true
-role [:web, :app, :db], %w{ 3.111.34.173 }, user: 'ubuntu', primary: true
+# small server
+# role [:web, :app, :db], %w{ 3.111.34.173 }, user: 'ubuntu', primary: true
+
+# big server - 3.110.191.3
+role [:web, :app, :db], %w{ 3.110.191.3 }, user: 'ubuntu', primary: true
