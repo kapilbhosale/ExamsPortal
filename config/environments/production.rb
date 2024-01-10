40654ded5e850890bc9ad9eec21bc8058618de05
kapilbhosale <kapilbhosale@hotmail.com>
2024-01-10 10:50:54 +0530

    assets cloudfront disabled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,15 +29,14 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # asset url only for RCC
-  # config.action_controller.asset_host = ENV.fetch('ASSETS_AWS_CLOUDFRONT_URL')
-  # config.serve_static_assets = true
+  config.action_controller.asset_host = ENV.fetch('ASSETS_AWS_CLOUDFRONT_URL')
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
