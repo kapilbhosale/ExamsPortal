f21e268b4ca74f56555b2db1bc808a1cd5f45d73
kapilbhosale <kapilbhosale@hotmail.com>
2024-05-16 17:07:39 +0530

    OMR import changes

--- app/workers/omr_import_worker.rb
+++ app/workers/omr_import_worker.rb
@@ -13,7 +13,7 @@ class OmrImportWorker
     REDIS_CACHE.set("omr-import-info-time", DateTime.now.strftime("%d-%B-%Y %I:%M%p"))
     REDIS_CACHE.set("omr-import-info-total_tests", Omr::Test.count)
     REDIS_CACHE.set("omr-import-info-last_test_name", Omr::Test.last&.test_name)
-    REDIS_CACHE.set("omr-import-info-last_test_desc", Omr::Test.last&.test_desc)
+    REDIS_CACHE.set("omr-import-info-last_test_desc", Omr::Test.last&.description)
     REDIS_CACHE.set("omr-import-info-last_test_date", Omr::Test.last&.test_date&.strftime("%d-%B-%Y %I:%M%p"))
   end
 end
