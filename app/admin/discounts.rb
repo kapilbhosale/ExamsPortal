3263ded10b676225d13635f7a91704195a8c52ae
kapilbhosale <kapilbhosale@hotmail.com>
2024-07-10 17:36:16 +0530

    discounts permission check added

--- app/admin/discounts.rb
+++ app/admin/discounts.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register Discount do
   permit_params :amount, :student_name, :parent_mobile, :student_mobile, :status, :data, :comment
 
+  actions :index, :show
 
   filter :amount
   filter :student_name
@@ -11,7 +12,7 @@ ActiveAdmin.register Discount do
   filter :created_at
 
 
-  index do
+  index row_class: ->(element) { 'highlight' if element.amount > 20 } do
     selectable_column
     id_column
     column :amount
@@ -25,33 +26,4 @@ ActiveAdmin.register Discount do
     column :data
     column :created_at
   end
-
-  form do |f|
-    f.inputs 'Org Admins' do
-      f.input :amount
-      f.input :student_name
-      f.input :parent_mobile
-      f.input :student_mobile
-      f.input :type_of_discount
-      f.input :status
-      f.input :data
-    end
-
-    f.actions
-  end
 end
-
-
-#  amount           :decimal(, )
-#  approved_by      :string
-#  comment          :string
-#  data             :jsonb
-#  parent_mobile    :string
-#  roll_number      :string
-#  status           :string
-#  student_mobile   :string
-#  student_name     :string
-#  type_of_discount :string
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
-#  org_id           :bigint(8)
