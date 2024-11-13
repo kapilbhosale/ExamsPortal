d9940ee2536c3cda63623f761beac264735e045c
kapilbhosale <kapilbhosale@hotmail.com>
2024-11-13 13:08:47 +0530

    index removed

--- db/migrate/20241112103856_add_unique_index_on_omr_batches.rb
+++ db/migrate/20241112103856_add_unique_index_on_omr_batches.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexOnOmrBatches < ActiveRecord::Migration[5.2]
   def change
-    add_index :omr_batches, [:name, :branch], unique: true, name: 'index_omr_batches_on_name_and_branch'
+    # add_index :omr_batches, [:name, :branch], unique: true, name: 'index_omr_batches_on_name_and_branch'
   end
 end
