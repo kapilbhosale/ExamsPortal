5da6859a038989f3301f1509927ae277453f04a6
kapilbhosale <kapilbhosale@hotmail.com>
2024-12-28 14:25:02 +0530

    Fees Batch api now only returns 2025-26 batches

--- app/controllers/admin/api/v2/batches_controller.rb
+++ app/controllers/admin/api/v2/batches_controller.rb
@@ -1,6 +1,10 @@
 class Admin::Api::V2::BatchesController < Admin::Api::V2::ApiController
   def index
     batch_ids = BatchFeesTemplate.where(batch_id: current_admin.batches.ids).pluck(:batch_id)
-    @batches = Batch.where(org_id: current_org.id).where(id: batch_ids).includes(:batch_group).order(:created_at)
+    @batches = Batch
+      .where(org_id: current_org.id)
+      .where(edu_year: current_org.data['current_academic_year'] || FeesTransaction::CURRENT_ACADEMIC_YEAR)
+      .where(id: batch_ids)
+      .includes(:batch_group).order(:created_at)
   end
 end
