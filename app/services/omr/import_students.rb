8398283b1638f088adaed0e6f036f59a5465642a
kapilbhosale <kapilbhosale@hotmail.com>
2025-01-10 18:30:48 +0530

    omr student import changed from bluk to iterative

--- app/services/omr/import_students.rb
+++ app/services/omr/import_students.rb
@@ -19,7 +19,7 @@ class Omr::ImportStudents
         # next if student_id is not a number or its greater than max_student_id
         next if student_id.to_i.to_s != student_id || student_id.to_i >= max_student_id
 
-        student = OMR::Student.find_by(
+        student = Omr::Student.find_by(
           roll_number: csv_row['Student_Roll_No'],
           org_id: org_id,
           parent_mobile: csv_row['Parent_Contact'].to_s.strip,
