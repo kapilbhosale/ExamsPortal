231b00c3811bc469c31b4183b72646716586aa4a
kapilbhosale <kapilbhosale@hotmail.com>
2025-03-25 21:32:28 +0530

    KCP fees import changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_03_05_130324) do
+ActiveRecord::Schema.define(version: 2025_03_25_160140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
